99a9702cf87394ec8f6a1c6895c59c1ec0d93e2a
Ziyi Wang <ziyiwang1027@gmail.com>
2021-09-24 19:48:54 +0200

    Amélioration controleur

--- src/controleur/ControleurCrise.php
+++ src/controleur/ControleurCrise.php
@@ -24,7 +24,6 @@ class ControleurCrise
         $liste = Utilisateurs::all();
         if (!is_null($liste)){
             $vue = new VuePrincipale([$liste], $this->container);
-            //$basePath = \Slim\Routing\RouteContext::fromRequest($rq)->getBasePath();
             $rs->getBody()->write($vue->getVueUser());
         }
         return $rs;
@@ -33,8 +32,7 @@ class ControleurCrise
 
     function getAccueil(Request $rq, Response $rs, array $args ): Response {
         $basePath = \Slim\Routing\RouteContext::fromRequest($rq)->getBasePath();
-        $routeContext = RouteContext::fromRequest($rq);
-        $routeParser = $routeContext->getRouteParser();
+        $routeParser = RouteContext::fromRequest($rq)->getRouteParser();
         //$relativeUrl = $routeParser->relativeUrlFor('inscription');
         $inscription = $routeParser->urlFor('inscription');
         $accueil = $routeParser->urlFor('accueil');
@@ -53,8 +51,7 @@ class ControleurCrise
 
     function getInscription(Request $rq, Response $rs, array $args ): Response {
         $basePath = \Slim\Routing\RouteContext::fromRequest($rq)->getBasePath();
-        $routeContext = RouteContext::fromRequest($rq);
-        $routeParser = $routeContext->getRouteParser();
+        $routeParser = RouteContext::fromRequest($rq)->getRouteParser();
         //$relativeUrl = $routeParser->relativeUrlFor('inscription');
         $inscription = $routeParser->urlFor('inscription');
         $accueil = $routeParser->urlFor('accueil');
@@ -72,8 +69,7 @@ class ControleurCrise
 
     function getConnexion(Request $rq, Response $rs, array $args ): Response {
         $basePath = \Slim\Routing\RouteContext::fromRequest($rq)->getBasePath();
-        $routeContext = RouteContext::fromRequest($rq);
-        $routeParser = $routeContext->getRouteParser();
+        $routeParser = RouteContext::fromRequest($rq)->getRouteParser();
         //$relativeUrl = $routeParser->relativeUrlFor('inscription');
         $inscription = $routeParser->urlFor('inscription');
         $accueil = $routeParser->urlFor('accueil');
