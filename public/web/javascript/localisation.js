33621a61e9b4ea3fed651bd251ad8d0a7089e227
Ziyi Wang <ziyiwang1027@gmail.com>
2021-10-26 18:26:34 +0200

    Ajout fonctionnalité

--- public/web/javascript/localisation.js
+++ public/web/javascript/localisation.js
@@ -1,14 +1,38 @@
+google.maps.event.addDomListener(window, 'load', initialize);
+
+listeContaminee = []
+function getListeLocal(){
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4) {
+            let listeLocal = this.responseText.split("{\"error\":\"Not found.\"}")[1]
+            JSON.parse(listeLocal).forEach(local => {
+                console.log(local)
+                listeContaminee.push(local)
+            })
+        }
+    }
+    xmlhttp.open('GET', 'public/web/script/localisation.php', false);
+    xmlhttp.send();
+}
+
 function initialize() {
     let mapProp = {
-        center:new google.maps.LatLng(48.684457, 6.163311),
-        zoom:4,
-        mapTypeId:google.maps.MapTypeId.ROADMAP
+        center: new google.maps.LatLng(48.684457, 6.163311),
+        zoom: 4,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-
-    position(48.684457, 6.163311)
-    position(50.684457, 7.163311)
-
+    getListeLocal()
+    listeContaminee.forEach(local => {
+/*
+        map.setCenter({
+            lat: Number(Lat),
+            lng: Number(Lng)
+        });
+*/
+        position(parseFloat(local.longitude), parseFloat(local.latitude))
+    })
 }
 
 function position(lat, lng){
@@ -18,5 +42,3 @@ function position(lat, lng){
         map: map,
     });
 }
-
-google.maps.event.addDomListener(window, 'load', initialize);
---
+++ public/web/script/localisation.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once '../../index.php';
+use crise\models\Localisations;
+
+echo Localisations::all();
